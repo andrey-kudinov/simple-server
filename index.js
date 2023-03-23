3dd6624c7d460f883ef84b71aa77e82dd4c9c02a
Andrey Kudinov <andrey.kudinov@avaloninnovations.com.au>
2023-03-23 23:46:29 +0400

    Add cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.post('/api/data', (req, res) => {
   console.log(req.body);
